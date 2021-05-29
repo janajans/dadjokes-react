50580cf9a89ab3984cf255279c1b1c678101cb3e
janajans <1janajansova@gmail.com>
2021-05-29 12:02:10 +0200

    tlačítka fungují, úkol 14 hotov

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-const App = () => (
-  <div className="container">
-    <div className="joke">
-      <div className="joke__body">
-        <div className="joke__user">
-          <img
-            className="user-avatar"
-            src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-          />
-          <p className="user-name">Neroxx</p>
-        </div>
+// čistý javascript:
+// let upLikes = 0;
+// let downLikes = 0;
 
-        <p className="joke__text">
-          The secret service isn't allowed to yell "Get down!" anymore when the
-          president is about to be attacked. Now they have to yell "Donald,
-          duck!"
-        </p>
+// const btnUp = document.querySelector('#btn-up');
+// const btnDown = document.querySelector('#btn-down');
+
+// btnUp.addEventListener('click', () => {
+//   upLikes += 1;
+//   const likesUpElm = document.querySelector('#likes-up');
+//   likesUpElm.textContent = upLikes;
+// });
+
+// btnDown.addEventListener('click', () => {
+//   downLikes += 1;
+//   const likesDownElm = document.querySelector('#likes-down');
+//   likesDownElm.textContent = downLikes;
+// });
+
+const App = () => {
+  const [likesUp, setLikesUp] = useState(0);
+  const [likesDown, setLikesDown] = useState(0);
+  return (
+    <>
+      <div className="container">
+        <div className="joke">
+          <div className="joke__body">
+            <div className="joke__user">
+              <img
+                className="user-avatar"
+                src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+              />
+              <p className="user-name">Neroxx</p>
+            </div>
+
+            <p className="joke__text">
+              The secret service isn't allowed to yell "Get down!" anymore when
+              the president is about to be attacked. Now they have to yell
+              "Donald, duck!"
+            </p>
+          </div>
+          <div className="joke__likes">
+            <button
+              id="btn-up"
+              className="btn-like btn-like--up"
+              onClick={() => setLikesUp(likesUp + 1)}
+            ></button>
+            <span id="likes-up" className="likes-count likes-count--up">
+              {likesUp}
+            </span>
+            <button
+              id="btn-down"
+              className="btn-like btn-like--down"
+              onClick={() => setLikesDown(likesDown + 1)}
+            ></button>
+            <span id="likes-down" className="likes-count likes-count--down">
+              {likesDown}
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="joke__likes">
-        <button id="btn-up" className="btn-like btn-like--up"></button>
-        <span id="likes-up" className="likes-count likes-count--up">
-          0
-        </span>
-        <button id="btn-down" className="btn-like btn-like--down"></button>
-        <span id="likes-down" className="likes-count likes-count--down">
-          0
-        </span>
-      </div>
-    </div>
-  </div>
-);
+    </>
+  );
+};
 
 render(<App />, document.querySelector('#app'));
